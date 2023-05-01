b1ee41da2117fb346401df2f91d3eaac8b46bdfa
topVAZ <topvaz@gmail.com>
2023-05-01 18:03:43 +0700

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,131 +1,126 @@
 "use strict";
-$(function() {
-	var $nav = $('nav.greedy');
-	var $btn = $('nav.greedy button');
-	var $vlinks = $('nav.greedy .links');
-	var $hlinks = $('nav.greedy .hidden-links');
-
-	var numOfItems = 0;
-	var totalSpace = 0;
-	var breakWidths = [];
-
-	// Get initial state
-	$vlinks.children().outerWidth(function(i, w) {
-		totalSpace += w;
-		numOfItems += 1;
-		breakWidths.push(totalSpace);
-	});
-
-	var availableSpace, numOfVisibleItems, requiredSpace;
-
-	function check() {
-
-		// Get instant state
-		availableSpace = $vlinks.width() - 10;
-		numOfVisibleItems = $vlinks.children().length;
-		requiredSpace = breakWidths[numOfVisibleItems - 1];
-
-		// There is not enought space
-		if (requiredSpace > availableSpace) {
-			$vlinks.children().last().prependTo($hlinks);
-			numOfVisibleItems -= 1;
-			check();
-			// There is more than enough space
-		} else if (availableSpace > breakWidths[numOfVisibleItems]) {
-			$hlinks.children().first().appendTo($vlinks);
-			numOfVisibleItems += 1;
-		}
-		// Update the button accordingly
-		$btn.attr("count", numOfItems - numOfVisibleItems);
-		if (numOfVisibleItems === numOfItems) {
-			$btn.addClass('hidden');
-		} else $btn.removeClass('hidden');
-	}
-
-	// Window listeners
-	$(window).resize(function() {
-		check();
-	});
-
-	$btn.on('click', function() {
-		$hlinks.toggleClass('hidden');
-	});
-
-	check();
-
-});
+var is_fullscreen = false;
 function open_fullscreen() {
 	let game = document.getElementById("game-area");
-	if (game.requestFullscreen) {
-		game.requestFullscreen();
-	} else if (game.mozRequestFullScreen) { /* Firefox */
-		game.mozRequestFullScreen();
-	} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-		game.webkitRequestFullscreen();
-	} else if (game.msRequestFullscreen) { /* IE/Edge */
-		game.msRequestFullscreen();
+	if(is_fullscreen){
+		// Exit fullscreen
+		is_fullscreen = false;
+		if(is_mobile_device()){
+			game.style.position = "absolute";
+			document.getElementById("mobile-back-button").style.display = "none";
+			document.getElementById("game-player").style.display = "none";
+		} else {
+			if (game.requestFullscreen) {
+				game.requestFullscreen();
+			} else if (game.mozRequestFullScreen) { /* Firefox */
+				game.mozRequestFullScreen();
+			} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+				game.webkitRequestFullscreen();
+			} else if (game.msRequestFullscreen) { /* IE/Edge */
+				game.msRequestFullscreen();
+			}
+		}
+	} else {
+		// Enter fullscreen
+		is_fullscreen = true;
+		if(is_mobile_device()){
+			document.getElementById("game-player").style.display = "block";
+			game.style.position = "fixed";
+			document.getElementById("mobile-back-button").style.display = "flex";
+		} else {
+			if (game.requestFullscreen) {
+				game.requestFullscreen();
+			} else if (game.mozRequestFullScreen) { /* Firefox */
+				game.mozRequestFullScreen();
+			} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+				game.webkitRequestFullscreen();
+			} else if (game.msRequestFullscreen) { /* IE/Edge */
+				game.msRequestFullscreen();
+			}
+		}
 	}
 };
-var can_resize = false;
-if($('iframe#game-area').length){
-	can_resize = true;
-	resize_game_iframe();
-	load_leaderboard({type: 'top', amount: 10});
-}
-function resize_game_iframe(){
-	if(can_resize){
-		let iframe = $("iframe.game-iframe");
-		let size = {
-			width: Number(iframe.attr('width')),
-			height: Number(iframe.attr('height')),
-		}
-		let ratio = (size.height/size.width)*100;
-		let win_ratio = (window.innerHeight/window.innerWidth)*100;
-		if(win_ratio <= 110){
-			if(ratio > 80){
-				ratio = 80;
-			}
-		} else if(win_ratio >= 130){
-			if(ratio < 100){
-				ratio = 100;
-			}
-		}
-		//console.log(ratio);
-		//console.log(win_ratio);
-		$('.game-iframe-container').css('padding-top', ratio+'%');
+function is_mobile_device(){
+	if (navigator.userAgent.match(/Android/i)
+	|| navigator.userAgent.match(/webOS/i)
+	|| navigator.userAgent.match(/iPhone/i)
+	|| navigator.userAgent.match(/iPad/i)
+	|| navigator.userAgent.match(/iPod/i)
+	|| navigator.userAgent.match(/BlackBerry/i)
+	|| navigator.userAgent.match(/Windows Phone/i)) {
+		return true;
+	} else {
+		return false;
 	}
+}
+if($('iframe#game-area').length){
+	load_leaderboard({type: 'top', amount: 10});
+	drag_back_btn(document.getElementById("mobile-back-button"));
+	if(is_mobile_device()){
+		if($('#allow_mobile_version').length){
+			document.getElementById('mobile-play').style.display = 'block';
+			document.getElementById('game-player').style.display = 'none';
+		}
+	}
+}
+function drag_back_btn(elem) {
+	let is_drag = false;
+	let pos_1 = 0, pos_2 = 0;
+	let last_y = elem.style.top;
+	let touchstart_y = 0;
+	elem.addEventListener("touchend", function(e) {
+		if(is_drag){
+			is_drag = false;
+		}
+	}, false);
+	elem.addEventListener("touchmove", function(e) {
+		e.preventDefault();
+		let touch = e.changedTouches[0];
+		if(!is_drag){
+			if(touchstart_y < touch.clientY+5 || touchstart_y > touch.clientY-5){
+				// Trigger "dragstart"
+				pos_2 = e.clientY;
+				is_drag = true;
+			}
+		}
+		if(is_drag){
+			pos_1 = pos_2 - touch.clientY;
+			pos_2 = touch.clientY;
+			elem.style.top = (pos_2) + "px";
+		}
+	}, false);
+	elem.addEventListener("touchstart", function(e) {
+		let touch = e.changedTouches[0];
+		last_y = elem.style.top;
+		touchstart_y = touch.clientY;
+	}, false);
+	elem.addEventListener("click", function(e) {
+		e.preventDefault();
+		if(last_y == elem.style.top){
+			open_fullscreen();
+		}
+	}, false);
 }
 function load_leaderboard(conf){
 	if($('#content-leaderboard').length){
-		let cur_url = $("iframe.game-iframe").attr('src');
-		let ref;
-		if(cur_url[cur_url.length-1] === '/'){
-			ref = cur_url.substring(
-			    cur_url.indexOf("/games/") + 7, 
-			    cur_url.length-1
-			);
-		} else if(cur_url.substr(cur_url.length-5, cur_url.length) === '.html') {
-			ref = cur_url.substring(
-			    cur_url.indexOf("/games/") + 7, 
-			    cur_url.lastIndexOf("/index.html")
-			);
-		}
-		let params = 'action=get_scoreboard&ref='+ref+'&conf='+JSON.stringify(conf);
-		let xhr = new XMLHttpRequest();
-		xhr.open('POST', '/includes/api.php', true);
-		xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-		xhr.onload = function() {
-			if (xhr.status === 200) {
-				if(xhr.responseText){
-					show_leaderboard(JSON.parse(xhr.responseText));
+		let g_id = $('#content-leaderboard').data('id');
+		$.ajax({
+			url: '/includes/api.php',
+			type: 'POST',
+			dataType: 'json',
+			data: {'action': 'get_scoreboard', 'game-id': g_id, 'conf': JSON.stringify(conf)},
+			complete: function (data) {
+				if(data.responseText){
+					if(JSON.parse(data.responseText).length){
+						show_leaderboard(JSON.parse(data.responseText));
+					}
 				}
 			}
-		}.bind(this);
-		xhr.send(params);
+		});
 	}
 }
 function show_leaderboard(data){
-	let html = '<table class="table"><thead class="thead-dark"><tr><th scope="col">#</th><th scope="col">Username</th><th scope="col">Score</th><th scope="col">Date</th></tr></thead><tbody>';
+	let html = '<table class="table table-striped table-dark"><thead class="thead-dark"><tr><th scope="col">#</th><th scope="col">Username</th><th scope="col">Score</th><th scope="col">Date</th></tr></thead><tbody>';
 	let index = 1;
 	data.forEach((item)=>{
 		html += '<tr><th scope="row">'+index+'</th><td>'+item.username+'</td><td>'+item.score+'</td><td>'+item.created_date.substr(0, 10)+'</td></tr>';
@@ -134,21 +129,83 @@ function show_leaderboard(data){
 	html += '</tbody></table>';
 	$('#content-leaderboard').html(html);
 }
-$(document).ready(()=>{
-	$("#navb").on('show.bs.collapse', function(){
-		$('.user-avatar').hide();
+(function(){
+	let last_offset = $("#new-games-section").children().length;
+	let load_amount = 12;
+	$('#load-more1').click((e)=>{
+		e.preventDefault();
+		$(this).addClass('disabled');
+		fetch_games(load_amount, 'new');
 	});
-	$("#navb").on('hidden.bs.collapse', function(){
-		$('.user-avatar').show();
-	});
-	resize_game_iframe();
-	$(window).resize(function() {
-		resize_game_iframe();
-	});
-	$('.stats-vote #upvote').on('click', function() {
-		let data_id = $(this).attr('data-id');
+	function fetch_games(amount, sort_by) {
 		$.ajax({
-			url: '../includes/vote.php',
+			url: "/includes/fetch.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {amount: amount, offset: last_offset, sort_by: sort_by},
+			complete: function (data) {
+				append_content(JSON.parse(data.responseText));
+			}
+		});
+	}
+	function append_content(data){
+		last_offset += data.length;
+		data.forEach((game)=>{
+			let rating = 0;
+			game['upvote'] = Number(game['upvote']);
+			game['downvote'] = Number(game['downvote']);
+			let total_revs = game['upvote']+game['downvote'];
+			if(total_revs > 0){
+				rating = (Math.round((game['upvote']/(game['upvote']+game['downvote'])) * 5) / 10);
+			}
+			let html = '<div class="col-md-2 col-sm-3 col-4 grid-1">';
+			html += '<a href="/game/'+game['slug']+'/">';
+			html += '<div class="game-item">'
+			html += '<div class="list-game">';
+			html += '<div class="list-thumbnail"><img src="'+game['thumb_2']+'" class="small-thumb img-rounded" alt="'+game['title']+'"></div>';
+			html += '<div class="list-info">';
+			html += '<div class="list-title">'+game['title']+'</div>';
+			html += '</div></div></div></a></div>';
+			$("#new-games-section").append(html);
+			$("#new-games-section .column:hidden").slice(0, load_amount).slideDown();
+		});
+		if(data.length < load_amount){
+			$("#load-more1").text("No More Games").addClass("noContent disabled");
+		} else {
+			$("#load-more1").removeClass('disabled');
+		}
+	}
+	$('#mobile-play-btn').on('click', function(e) {
+		open_fullscreen();
+	});
+	$('.b-action #favorite').on('click', function(e) {
+		e.preventDefault();
+		let data_id = $('.game-content').attr('data-id');
+		let btn = $(this);
+		$.ajax({
+			url: '/includes/vote.php',
+			type: 'POST',
+			dataType: 'json',
+			data: {'favorite': true, 'action': 'favorite', 'id': data_id},
+			success: function (data) {
+				//console.log(data.responseText);
+			},
+			error: function (data) {
+				//console.log(data.responseText);
+			},
+			complete: function (data) {
+				console.log(data.responseText);
+				btn.addClass('active');
+				btn.addClass('disabled');
+			}
+		});
+	});
+	$('.b-action #upvote').on('click', function(e) {
+		e.preventDefault();
+		let data_id = $('.game-content').attr('data-id');
+		let btn = $(this);
+		$.ajax({
+			url: '/includes/vote.php',
 			type: 'POST',
 			dataType: 'json',
 			data: {'vote': true, 'action': 'upvote', 'id': data_id},
@@ -160,17 +217,17 @@ $(document).ready(()=>{
 			},
 			complete: function (data) {
 				console.log(data.responseText);
-				$('.icon-vote').hide();
-				let elem = $('.vote-status');
-				elem.addClass('text-success');
-				elem.append('Liked!');
+				btn.addClass('active');
+				btn.addClass('disabled');
 			}
 		});
 	});
-	$('.stats-vote #downvote').on('click', function() {
-		let data_id = $(this).attr('data-id');
+	$('.b-action #downvote').on('click', function(e) {
+		e.preventDefault();
+		let data_id = $('.game-content').attr('data-id');
+		let btn = $(this);
 		$.ajax({
-			url: '../includes/vote.php',
+			url: '/includes/vote.php',
 			type: 'POST',
 			dataType: 'json',
 			data: {'vote': true, 'action': 'downvote', 'id': data_id},
@@ -182,15 +239,14 @@ $(document).ready(()=>{
 			},
 			complete: function (data) {
 				console.log(data.responseText);
-				$('.icon-vote').hide();
-				let elem = $('.vote-status');
-				elem.addClass('text-danger');
-				elem.append('Disliked!');
+				btn.addClass('active');
+				btn.addClass('disabled');
 			}
 		});
 	});
-	$('.user-avatar').on('click', ()=>{
-		let element = $('.user-links');
+	$('.user-avatar').on('click', function(){
+		
+		let element = $(this).next();
 		if (element.is(":hidden")) {
 			element.removeClass('hidden');
 		} else element.addClass('hidden');
@@ -200,16 +256,36 @@ $(document).ready(()=>{
 			scrollLeft: '-=150'
 		}, 300, 'swing');
 	});
-	
 	$('#btn_next').on('click', function() {
 		$('.profile-gamelist ul').animate({
+			scrollLeft: '+=150'
+		}, 300, 'swing');
+	});
+	$('#f_prev').on('click', function() {
+		$('.favorite-gamelist ul').animate({
+			scrollLeft: '-=150'
+		}, 300, 'swing');
+	});
+	$('#f_next').on('click', function() {
+		$('.favorite-gamelist ul').animate({
+			scrollLeft: '+=150'
+		}, 300, 'swing');
+	});
+	$('#t-prev').on('click', function() {
+		$('.list-1-wrapper').animate({
+			scrollLeft: '-=150'
+		}, 300, 'swing');
+	});
+	
+	$('#t-next').on('click', function() {
+		$('.list-1-wrapper').animate({
 			scrollLeft: '+=150'
 		}, 300, 'swing');
 	});
 	$('.delete-comment').on('click', function() {
 		let id = $(this).attr('data-id');
 		$.ajax({
-			url: '../includes/comment.php',
+			url: '/includes/comment.php',
 			type: 'POST',
 			dataType: 'json',
 			data: {'delete': true, 'id': id},
@@ -220,7 +296,6 @@ $(document).ready(()=>{
 				//console.log(data.responseText);
 			},
 			complete: function (data) {
-				console.log(data.responseText);
 				if(data.responseText === 'deleted'){
 					$('.id-'+id).remove();
 				}
@@ -229,9 +304,9 @@ $(document).ready(()=>{
 	});
 	let game_id;
 	if($('#comments').length){
-		game_id = $('.stats-vote #upvote').attr('data-id');
+		game_id = $('.game-content').attr('data-id');
 		$.ajax({
-			url: '../includes/comment.php',
+			url: '/includes/comment.php',
 			type: 'POST',
 			dataType: 'json',
 			data: {'load': true, 'game_id': game_id},
@@ -289,7 +364,7 @@ $(document).ready(()=>{
 				commentJSON.game_id = game_id;
 				$.ajax({
 					type: 'post',
-					url: '../includes/comment.php',
+					url: '/includes/comment.php',
 					data: commentJSON,
 					success: function(comment) {
 						console.log(comment);
@@ -300,4 +375,4 @@ $(document).ready(()=>{
 			}
 		});
 	}
-});
+})();
